bfba68ff36b7b55f7cecbdc5037ce9957344625a
DOH-FAA3303 <frank.aragona@doh.wa.gov>
2024-08-20 14:19:34 -0700

    fix: rename onload package

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,11 @@
 #'
 #' @importFrom reticulate import use_virtualenv configure_environment
+
+transformers <- NULL
+
 .onLoad <- function(libname, pkgname){
   
-  # transformers <<- reticulate::import('transformers', delay_load = TRUE)
+  transformers <<- reticulate::import('transformers', delay_load = TRUE)
   reticulate::use_virtualenv("r-translatemd",required = FALSE)
   reticulate::configure_environment(pkgname)
 
