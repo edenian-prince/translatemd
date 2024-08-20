e994c70e4d0675f24f338c451154950e5ed69426
DOH-FAA3303 <frank.aragona@doh.wa.gov>
2024-08-20 14:33:25 -0700

    fix: remove pipeline as a reticulate dependancy

--- R/select_lang.R
+++ R/select_lang.R
@@ -3,7 +3,7 @@
 #' for example "Helsinki-NLP/opus-mt-en-es"
 #' @export
 #' @param lang
-#' @importFrom reticulate import pipeline
+#' @importFrom reticulate import
 select_lang <- function(lang){
   translator <- transformers$pipeline(task = "translation", model=lang)
 }
